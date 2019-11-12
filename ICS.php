abbc4dcb3140af54e36ef4d323bee2e4b91437c5
greg-randall <gregrr@gmail.com>
2019-11-12 12:56:27 -0600

    Update ICS.php
    Changed the date code to be current time zone rather than GMT.

--- ICS.php
+++ ICS.php
@@ -1,12 +1,38 @@
 <?php
 
 /**
+ * This is free and unencumbered software released into the public domain.
+ * 
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <http://unlicense.org>
+ * 
  * ICS.php
- * =======
+ * =============================================================================
  * Use this class to create an .ics file.
+ * 
  *
  * Usage
- * -----
+ * -----------------------------------------------------------------------------
  * Basic usage - generate ics file contents (see below for available properties):
  *   $ics = new ICS($props);
  *   $ics_file_contents = $ics->to_string();
@@ -22,7 +48,7 @@
  *   ));
  *
  * Available properties
- * --------------------
+ * -----------------------------------------------------------------------------
  * description
  *   String description of the event.
  * dtend
@@ -41,7 +67,7 @@
  */
 
 class ICS {
-  const DT_FORMAT = 'Ymd\THis\Z';
+  const DT_FORMAT = 'Ymd\THis';
 
   protected $properties = array();
   private $available_properties = array(
